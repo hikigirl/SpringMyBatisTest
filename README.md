72b462408a2d1268a8c7bed5df9672da9bfa21b4
Sunmin <sunmin323233@gmail.com>
2025-10-22 11:21:21 +0900

    MyBatisTest initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# MyBatisTest
+
+## 데이터베이스 작업 관련
+
+- new - spring legacy project - spring mvc project
+- project name: `MyBatisTest`
+- root package: `com.test.java`
+
+--
+
+#### 자바 버전과 pom.xml 수정
+- 프로젝트 우클릭 -> Project Facets -> Java 11
+- 
+
+
+
+#### DB작업
+- JetBrains Datagrip 설치해보기(dbeaver같은 툴임)
+- 프로젝트 루트에 script.sql 생성
+
+--
+
+#### 패키지
+
---
+++ script.sql
@@ -0,0 +1,20 @@
+--계정 생성(system 계정으로 실행)
+alter session set "_ORACLE_SCRIPT" = true;
+create user spring identified by java1234;
+grant connect, resource, dba to spring;
+alter user spring default tablespace users;
+
+select * from tabs;
+
+create table tblAddress(
+    seq number primary key,
+    name varchar2(30) not null,
+    age number(3) not null,
+    address varchar2(300) not null,
+    gender char(1) not null
+);
+create sequence seqAddress;
+select * from tblAddress;
+insert into tblAddress
+    values (seqAddress.nextval, '홍길동', 20, '서울시 강남구 대치동', 'm');
+commit;
